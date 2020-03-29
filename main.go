380bac015d9fa0e2130dcf58d88aad703a3c2498
genshen <genshenchu@gmail.com>
2020-03-29 12:21:50 +0800

    feat(cli): add config path option to cli

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"flag"
 	"github.com/genshen/ssh-web-console/src/routers"
 	"github.com/genshen/ssh-web-console/src/utils"
 	"log"
 	"net/http"
 )
 
+var confFilePath string
+
+func init() {
+	flag.StringVar(&confFilePath, "config", "conf/config.yaml", "filepath of config file.")
+}
+
 func main() {
-	if err := utils.InitConfig("conf/config.yaml"); err != nil {
+	flag.Parse()
+	if err := utils.InitConfig(confFilePath); err != nil {
 		log.Fatal("config error,", err)
 		return
 	}
 	routers.Register()
-	log.Println("listening on port ",utils.Config.Site.ListenAddr)
+	log.Println("listening on port ", utils.Config.Site.ListenAddr)
 	// listen http
 	if err := http.ListenAndServe(utils.Config.Site.ListenAddr, nil); err != nil {
 		log.Fatal(err)
